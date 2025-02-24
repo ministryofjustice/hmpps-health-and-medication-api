fb5728b6b1207d3e8110d522195cdb80d0143dd1
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2025-02-24 16:40:15 +0000

    CDPS-1213: Conditional add role to DPR user if they exist.

--- src/main/resources/db/migration/common/V13__update_dpr_user_permissions.sql
+++
@@ -1,3 +0,0 @@
--- Grant additional permissions for DPR user to setup replication
-GRANT rds_superuser to ${dpr_user};
-GRANT rds_replication to ${dpr_user};
--- src/main/resources/db/migration/test/V12_1__create_rds_roles.sql
+++
@@ -1,2 +0,0 @@
-CREATE ROLE rds_superuser NOLOGIN;
-CREATE ROLE rds_replication NOLOGIN;
