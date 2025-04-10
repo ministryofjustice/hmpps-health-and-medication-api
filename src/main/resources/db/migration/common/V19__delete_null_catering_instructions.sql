1f6a78342dbbd493d40724a8bf216be24ce1e082
Scott Rowley <16266605+scottrowley@users.noreply.github.com>
2025-04-10 11:36:43 +0100

    CDPS-1354 Remove blank catering instructions

--- src/main/resources/db/migration/common/V19__delete_null_catering_instructions.sql
+++ src/main/resources/db/migration/common/V19__delete_null_catering_instructions.sql
@@ -1,3 +1,3 @@
-DELETE FROM catering_instructions WHERE instructions IS NULL;
+DELETE FROM catering_instructions WHERE instructions IS NULL OR trim(instructions) = '';
 
 ALTER TABLE catering_instructions ALTER COLUMN instructions SET NOT NULL;
