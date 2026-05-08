60ae7b1c47631e2af675fe054e6887a7252fea3b
Jon Brighton <brightonsbox@hotmail.com>
2026-05-08 10:37:59 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
   // OpenAPI
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
   constraints {
-    implementation("org.webjars:swagger-ui:5.32.5")
+    implementation("org.webjars:swagger-ui:5.32.2")
   }
 
   // UUIDs
