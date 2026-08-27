1bbc4f04d9acab8e50bfaf3731beb897b2afa318
Igor <igor.ushkarev1@justice.gov.uk>
2026-08-27 11:37:01 +0100

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("io.sentry:sentry-spring-boot-4:8.53.0")
+  implementation("io.sentry:sentry-spring-boot-4:8.54.0")
 
   // AWS
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.4.1")
