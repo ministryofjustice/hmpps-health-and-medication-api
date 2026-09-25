944fbe8737dec18ad5f802c3c2246b8f35e55cac
Jon Brighton <brightonsbox@hotmail.com>
2026-09-25 11:06:25 +0100

    updating gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("io.sentry:sentry-spring-boot-4:8.57.0")
+  implementation("io.sentry:sentry-spring-boot-4:8.58.0")
 
   // AWS
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.4.1")
